f726bda7100b628c86716e4bcc4295df52c3cf2d
Teddy <teddywambua5456@gmail.com>
2023-04-01 16:31:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+    const formData = {
+      name: name,
+      email: email
+    };
+    
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+    
+    return fetch("http://localhost:3000/users", configObj)
+      .then(response => response.json())
+      .then(data => {
+        const newId = data.id;
+        document.body.innerHTML += newId;
+      })
+      .catch(error => {
+        document.body.innerHTML += error.message;
+      });
+  }
+  
